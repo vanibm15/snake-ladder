21a29bf2cd0c55b0855043424212cacaa877ee19
vanibm15 <vanibm15@gmail.com>
2022-04-21 17:24:43 +0530

    added:player checks for option(snake/noplay/ladder)

--- src/com/bridgelabz/Snake_Ladder.java
+++ src/com/bridgelabz/Snake_Ladder.java
@@ -3,13 +3,33 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Snake_Ladder {
-    private static int getDieRoll() {
-        Random random = new Random();
-        int roll = random.nextInt(6) + 1;
-        System.out.println(roll);
-        return roll;
-    }
+
+
     public static void main(String[] args) {
-        int dieNumber = getDieRoll();
+        Random random = new Random();
+        int dieNum = random.nextInt(6) + 1;
+        int options = random.nextInt(4);
+        int position = 0;
+        final int LADDER = 1;
+        final int NOPLAY = 2;
+        final int SNAKE = 3;
+        switch (options) {
+            case LADDER:
+                position += dieNum;
+                System.out.println("the player got ladder:" + position);
+                break;
+            case NOPLAY:
+                System.out.println("the player remains in same position:" + position);
+                break;
+            case SNAKE:
+                position -= dieNum;
+                System.out.println("the snake bit the player:" + position);
+                break;
+        }
+        System.out.println("position is :" + position);
+
     }
+
 }
+
+
