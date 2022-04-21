0e477f351a0b5f7c0d78b3e47ab9896db017e21d
vanibm15 <vanibm15@gmail.com>
2022-04-21 18:02:24 +0530

    added:player reaches end point

--- src/com/bridgelabz/Snake_Ladder.java
+++ src/com/bridgelabz/Snake_Ladder.java
@@ -7,29 +7,34 @@ public class Snake_Ladder {
 
     public static void main(String[] args) {
         Random random = new Random();
-        int dieNum = random.nextInt(6) + 1;
-        int options = random.nextInt(4);
         int position = 0;
         final int LADDER = 1;
         final int NOPLAY = 2;
         final int SNAKE = 3;
-        switch (options) {
-            case LADDER:
-                position += dieNum;
-                System.out.println("the player got ladder:" + position);
-                break;
-            case NOPLAY:
-                System.out.println("the player remains in same position:" + position);
-                break;
-            case SNAKE:
-                position -= dieNum;
-                System.out.println("the snake bit the player:" + position);
-                break;
+        while (position < 100) {
+            int dieNum = random.nextInt(6) + 1;
+            int options = random.nextInt(4);
+            {
+
+                switch (options) {
+                    case LADDER:
+                        if (position + dieNum > 100)
+                            break;
+                        position += dieNum;
+                        break;
+                    case NOPLAY:
+                        break;
+                    case SNAKE:
+                        position -= dieNum;
+                        position = Math.max(position, 0);
+                        break;
+                }
+            }
+            System.out.println("position is :" + position);
         }
-        System.out.println("position is :" + position);
-
     }
-
 }
+
+
 
 
