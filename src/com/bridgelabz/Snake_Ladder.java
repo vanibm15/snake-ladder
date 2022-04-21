d76f2b2c50fd05b162d8e2e387dd0c5e8bbe2aa3
vanibm15 <vanibm15@gmail.com>
2022-04-21 15:53:16 +0530

    added:to get random no:to get1_6

--- src/com/bridgelabz/Snake_Ladder.java
+++ src/com/bridgelabz/Snake_Ladder.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Snake_Ladder {
+    private static int getDieRoll() {
+        Random random = new Random();
+        int roll = random.nextInt(6) + 1;
+        System.out.println(roll);
+        return roll;
+    }
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("game played at start position:" + position);
+        int dieNumber = getDieRoll();
     }
 }
